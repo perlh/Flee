4ad01f6058c0c1cfb6a37cbbebe40f4b7bbc9de1
hsm <hsmcool@163.com>
2023-05-27 01:53:35 +0800

    fix bug and add more function

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Flee 插件
-- flee3.0
+- flee3.1
 - 一款Chrome 浏览器插件。
 - 目前内置解码小工具，支持md5、base64、url、16进制转字符串、Unicode和UTF-8等编码。
 - 可用于CTF比赛。
 
+## Flee 3.1 更新内容
+- 修复上一个版本的bug。
+- 优化ui。
+- 对于“编码”功能页面添加监听功能，每次输入新的数据自动生成编码。
+- 对于“编码”功能页面添加数据本地存储功能，代码架构进行了大部分重构。
 
 ## 支持的编码
 
@@ -29,11 +34,24 @@ git clone https://github.com/perlh/Flee.git
 
 2. Chrome扩展程序 -> 加载已解压扩展程序 -> 选择`Flee`文件夹
 3. 确定
-## 插件UI界面
+
+## Flee_v3.1插件新UI界面
+
+#### 1、常用
+![EasyETools](docs/flee_index_3_1.png)
+
+#### 2、 编码
+![EasyETools](docs/flee_encoding_3_1.png)
+
+
+#### 3、加密
+
+![EasyETools](docs/flee_crypto_3_1.png)
+
+## Flee_v3.0插件旧UI界面
 
 #### 1、常用
 ![EasyETools](docs/flee3.png)
-![EasyETools](docs/flee3_2.png)
 #### 2、 编码
 ![EasyETools](docs/flee3_decode.png)
 
--- static/js/lib/decode.js
+++ static/js/lib/decode.js
@@ -1,261 +1,416 @@
-// 编码
-function mainEncode() {
-  let select_id = $("#decode-select").val();
-  // 输入框
-  let decode_input = $("#decode-input").val();
-  // 输出狂
-  var decode_output = $("#decode-output");
-  // 清空输出框
-  decode_output.val("");
-  if(decode_input == ""){
-    return ;
+// 关于本地存储的相关代码
+
+// 保存数据到本地存储
+function saveToLocalStorage(key, value) {
+  localStorage.setItem(key, value);
+}
+// 从本地存储中获取数据
+function getFromLocalStorage(key) {
+  var value = localStorage.getItem(key);
+  if (value) {
+    return value;
   }
-  //
-  // var wordArray = CryptoJS.enc.Utf8.parse(decode_input);
-  switch (select_id) {
-    case "0": {
-      const option = {
-        space: " ",
-        long: "-",
-        short: ".",
-      };
-      try {
-        decode = xmorse.encode(decode_input, option);
-      } catch (error) {
-        decode = error;
-      }
-      decode_output.val(decode);
-      break;
-    }
-    case "1": {
-      try {
-        var wordArray = CryptoJS.enc.Utf8.parse(decode_input);
-        // var base64 =
-        encode = CryptoJS.enc.Base64.stringify(wordArray);
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "2": {
-      // CryptoJS.MD5('hello').toString();
-      try {
-        encode = CryptoJS.MD5(decode_input).toString();
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "3": {
-      // CryptoJS.MD5('hello').toString();
-      try {
-        encode = CryptoJS.SHA256(decode_input).toString();
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "4": {
-      // CryptoJS.MD5('hello').toString();
-      try {
-        encode = CryptoJS.SHA224(decode_input).toString();
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "5": {
-      // CryptoJS.MD5('hello').toString();
-      try {
-        encode = CryptoJS.SHA512(decode_input).toString();
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "6": {
-      // CryptoJS.MD5('hello').toString();
-      try {
-        encode = CryptoJS.SHA384(decode_input).toString();
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "7": {
-      // CryptoJS.MD5('hello').toString();
-      try {
-        encode = CryptoJS.SHA1(decode_input).toString();
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "8": {
-      // CryptoJS.MD5('hello').toString();
-      try {
-        encode = CryptoJS.SHA3(decode_input).toString();
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "9": {
-      /*
-      Base32
-      */
-      try {
-        encode = base32.encode(decode_input);
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "10": {
-      try {
-        encode = encodeURIComponent(decode_input);
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "11": {
-      try {
-        encode = strToHexCharCode(strToUtf8Bytes(decode_input));
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "12": {
-      // unicode(str);
-      try {
-        encode = unicode(decode_input);
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "13": {
-      try {
-        encode = escape(decode_input).replace(/(%u)(\w{4})/gi, "&#x$2;");
-      } catch (error) {
-        encode = error;
-      } finally {
-        decode_output.val(encode);
-        break;
-      }
-    }
-    default:
-      break;
-  }
+  return null;
 }
 
-// 解码
-function mainDncode() {
-  let select_id = $("#decode-select").val();
-  // 输入框
-  let decode_input = $("#decode-input").val();
-  // 输出狂
-  var decode_output = $("#decode-output");
-  // 清空输出框
-  // var wordArray = CryptoJS.enc.Utf8.parse(decode_input);
-  switch (select_id) {
-    case "0": {
-      const option = {
-        space: " ",
-        long: "-",
-        short: ".",
-      };
-      try {
-        encode = xmorse.decode(decode_input, option);
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-
-    case "1": {
-      try {
-        var parsedWordArray = CryptoJS.enc.Base64.parse(decode_input);
-        encode = parsedWordArray.toString(CryptoJS.enc.Utf8);
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "9": {
-      try {
-        encode = base32.decode(decode_input);
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "10": {
-      try {
-        encode = decodeURIComponent(decode_input);
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "11": {
-      try {
-        encode = hexToString(decode_input);
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "12": {
-      // unicode(str);
-      try {
-        encode = unicodeToChar(decode_input);
-      } catch (error) {
-        encode = error;
-      }
-      decode_output.val(encode);
-      break;
-    }
-    case "13": {
-      var init_str;
-      try {
-        encode = unescape(decode_input.replace(/&#x/g, "%u").replace(/;/g, ""));
-      } catch (error) {
-        encode = error;
-      } finally {
-        decode_output.val(encode);
-        break;
-      }
-    }
-    default:
-      break;
-  }
+// 从本地存储中移除数据
+function removeFromLocalStorage(key) {
+  localStorage.removeItem(key);
 }
 
-let button_encode = document.getElementById("button-encode");
-button_encode.onclick = mainEncode;
+// 清空本地存储中的所有数据
+function clearLocalStorage() {
+  localStorage.clear();
+}
 
-let button_decode = document.getElementById("button-decode");
-button_decode.onclick = mainDncode;
 
-// a = ;
-// alert(string2byte("hello world"));
+// base64
+function flee_base64_decode(flee_string) {
+  try {
+    var parsedWordArray = CryptoJS.enc.Base64.parse(flee_string);
+    encode = parsedWordArray.toString(CryptoJS.enc.Utf8);
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+
+function flee_base64_encode(flee_string) {
+  try {
+    var wordArray = CryptoJS.enc.Utf8.parse(flee_string);
+    encode = CryptoJS.enc.Base64.stringify(wordArray);
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+
+function flee_base32_decode(flee_string) {
+  try {
+    encode = base32.decode(flee_string);
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+
+function flee_base32_encode(flee_string) {
+  try {
+    encode = base32.encode(flee_string);
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+function flee_morse_decode(flee_string) {
+  const option = {
+    space: " ",
+    long: "-",
+    short: ".",
+  };
+  try {
+    encode = xmorse.decode(flee_string, option);
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+  // decode_output.val(encode);
+}
+
+function flee_morse_encode(flee_string) {
+  const option = {
+    space: " ",
+    long: "-",
+    short: ".",
+  };
+  
+  try {
+    decode = xmorse.encode(flee_string, option);
+  } catch (error) {
+    decode = error;
+  }
+  // alert(encode);
+  return decode;
+}
+
+// function flee_base32_decode(flee_string) {
+
+// }
+
+function flee_md5_encode(flee_string) {
+  try {
+    encode = CryptoJS.MD5(flee_string).toString();
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+// function flee_sha1_decode(flee_string) {
+
+// }
+
+function flee_sha1_encode(flee_string) {
+  try {
+    encode = CryptoJS.SHA1(flee_string).toString();
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+// function flee_sha3_decode(flee_string) {
+
+// }
+
+function flee_sha3_encode(flee_string) {
+  try {
+    encode = CryptoJS.SHA3(flee_string).toString();
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+
+// function flee_sha224_decode(flee_string) {
+
+// }
+
+function flee_sha224_encode(flee_string) {
+  try {
+    encode = CryptoJS.SHA224(flee_string).toString();
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+// function flee_sha256_decode(flee_string) {
+
+// }
+
+function flee_sha256_encode(flee_string) {
+  try {
+    encode = CryptoJS.SHA256(flee_string).toString();
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+// function flee_sha384_decode(flee_string) {
+
+// }
+
+function flee_sha384_encode(flee_string) {
+  try {
+    encode = CryptoJS.SHA384(flee_string).toString();
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+
+function flee_sha512_encode(flee_string) {
+  try {
+    encode = CryptoJS.SHA512(flee_string).toString();
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+
+function flee_string2hex_decode(flee_string) {
+  try {
+    encode = hexToString(flee_string);
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+function flee_string2hex_encode(flee_string) {
+  try {
+    encode = strToHexCharCode(strToUtf8Bytes(flee_string));
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+
+function flee_url_encode(flee_string) {
+  try {
+    encode = encodeURIComponent(flee_string);
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+function flee_url_decode(flee_string) {
+  try {
+    encode = decodeURIComponent(flee_string);
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+function flee_unicode_encode(flee_string) {
+  try {
+    encode = unicode(flee_string);
+  } catch (error) {
+    encode = error;
+  }
+
+  return encode;
+}
+function flee_unicode_decode(flee_string) {
+  try {
+    encode = unicodeToChar(flee_string);
+  } catch (error) {
+    encode = error;
+  }
+
+  return encode;
+}
+function flee_utf8_encode(flee_string) {
+  try {
+    encode = escape(flee_string).replace(/(%u)(\w{4})/gi, "&#x$2;");
+  } catch (error) {
+    encode = error;
+  }
+  // alert(encode);
+  return encode;
+}
+function flee_utf8_decode(flee_string) {
+  try {
+    encode = unescape(flee_string.replace(/&#x/g, "%u").replace(/;/g, ""));
+  } catch (error) {
+    encode = error;
+  }
+  return encode;
+}
+
+
+function onEmptyStart(){
+  removeFromLocalStorage("flee_encoding_input");
+  removeFromLocalStorage("flee_encoding_opration");
+  removeFromLocalStorage("flee_encoding_select");
+  // 清空输出框
+  // clearForm();
+  // 清空输入框
+  $("#decode-input").val("");
+  $("#output_textarea").val("");
+      // 显示编码成功
+  // $('#status').css('display', 'none');
+  // $('#input_status').text("请在上方第一个文本框中输入要编码/解码的字符。");
+}
+
+// 主函数
+function flee_main() {
+  var opration = $("#opration").val();
+
+  // var input_textarea = $("#decode-input").val();
+  let input_textarea = $("#decode-input").val();
+
+  // alert(input_textarea);
+  // 获取选择的值
+  var flee_encoding_select = $("#decode-select").val();
+  // 获取输出框对象
+  var output_textarea = $("#output_textarea");
+  saveToLocalStorage("flee_encoding_input", input_textarea);
+  saveToLocalStorage("flee_encoding_opration", opration);
+  saveToLocalStorage("flee_encoding_select", flee_encoding_select);
+  var output_result = "";
+  // $("#output_textarea").css('color', 'dark');
+  if (opration === "encode") {
+    // encode
+    switch (flee_encoding_select) {
+      case "base64": {
+        // alert("base64 encode");
+        output_result = flee_base64_encode(input_textarea);
+        break;
+      }
+      case "base32": {
+        output_result = flee_base32_encode(input_textarea);
+        break;
+      }
+      case "morse": {
+        output_result = flee_morse_encode(input_textarea);
+        break;
+      }
+      case "md5": {
+        output_result = flee_md5_encode(input_textarea);
+        break;
+      }
+      case "sha1": {
+        output_result = flee_sha1_encode(input_textarea);
+        break;
+      }
+      case "sha3": {
+        output_result = flee_sha3_encode(input_textarea);
+        break;
+      }
+      case "sha224": {
+        output_result = flee_sha224_encode(input_textarea);
+        break;
+      }
+      case "sha256": {
+        output_result = flee_sha256_encode(input_textarea);
+        break;
+      }
+      case "sha384": {
+        output_result = flee_sha384_encode(input_textarea);
+        break;
+      }
+      case "sha512": {
+        output_result = flee_sha512_encode(input_textarea);
+        break;
+      }
+      case "string2hex": {
+        output_result = flee_string2hex_encode(input_textarea);
+        break;
+      }
+      case "url": {
+        output_result = flee_url_encode(input_textarea);
+        break;
+      }
+      case "utf8": {
+        output_result = flee_utf8_encode(input_textarea);
+        break;
+      }
+      case "unicode": {
+        output_result = flee_unicode_encode(input_textarea);
+        break;
+      }
+      default: {
+        output_result = "Encode not exist！";
+        // $("#output_textarea").css('color', 'red');
+      }
+    }
+  } else {
+    // decode
+    switch (flee_encoding_select) {
+      case "base64": {
+        // alert("base64 decode");
+        output_result = flee_base64_decode(input_textarea);
+        break;
+      }
+      case "base32": {
+        output_result = flee_base32_decode(input_textarea);
+        break;
+      }
+      case "morse": {
+        output_result = flee_morse_decode(input_textarea);
+        break;
+      }
+      case "string2hex": {
+        output_result = flee_string2hex_decode(input_textarea);
+        break;
+      }
+      case "url": {
+        output_result = flee_url_decode(input_textarea);
+        break;
+      }
+      case "utf8": {
+        output_result = flee_utf8_decode(input_textarea);
+        break;
+      }
+      case "unicode": {
+        output_result = flee_unicode_decode(input_textarea);
+        break;
+      }
+      default: {
+        output_result = "Decode Not exist！";
+        // $("#output_textarea").css('color', 'red');
+      }
+    }
+  }
+
+  // alert(output_result);
+  output_textarea.val(output_result);
+  // alert("ddd");
+  // $("#output_textarea").text("ddd");
+}
+
+// 立即执行
+$(document).ready(function () {
+  let onEmpty = document.getElementById("onEmpty");
+  onEmpty.onclick = onEmptyStart;
+  flee_encoding_input = getFromLocalStorage("flee_encoding_input");
+  flee_encoding_select = getFromLocalStorage("flee_encoding_select");
+  flee_encoding_opration = getFromLocalStorage("flee_encoding_opration");
+  // $('#selectId').val('optionValue');
+  if (flee_encoding_input != null) {
+    $("#decode-input").val(flee_encoding_input);
+    $("#decode-select").val(flee_encoding_select);
+    $("#opration").val(flee_encoding_opration);
+    flee_main();
+  }
+  // 每次打开页面将自动编码sdfsd
+  // active_decode_or_encode();
+  // 监听选择框的变化事件
+  $("#decode-select").change(flee_main);
+  $("#opration").change(flee_main);
+  // $("#decode-input").change(flee_main);
+  $("#decode-input").on("input", function () {
+    // var inputData = $(this).val(); // 获取输入框的值
+    // 执行你的函数，例如：
+    // alert("hello world");
+    flee_main();
+  });
+});
